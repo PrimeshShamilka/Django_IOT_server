2dbbc895918313ecc1ee959e63c51af4425f5964
Primesh <primeshs.17@cse.mrt.ac.lk>
2019-05-18 09:28:08 +0530

    multiple default values for devices table id column error 3 commit

--- webapp/models.py
+++ webapp/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class devices(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     device_name = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
     user_email = models.CharField(max_length=100)
     local_ip= models.CharField(max_length=100,null=True)
     mac_address=models.CharField(max_length=100,null=True)
-    
+
     def __str__(self):
           return self.device_name
 
-    # def save(self, *args, **kwargs):
-    #     #Check the MAC address and update database
-    #     flag = devices.objects.all().filter(mac_address=self.mac_address)
-    #     if not flag:
-    #         # Calling the superclass method to save data
-    #         super().save(*args, **kwargs)
-    #         print ("SAVED") 
-    #     else:
-    #         print ("NOT SAVED")   
-            
-            
+    def save(self, *args, **kwargs):
+        #Check the MAC address and update database
+        flag = devices.objects.all().filter(mac_address=self.mac_address)
+        if not flag:
+            # Calling the superclass method to save data
+            super().save(*args, **kwargs)
+            print ("SAVED")
+        else:
+            print ("NOT SAVED")
+
+
 class data(models.Model):
     device = models.ForeignKey(devices, on_delete=models.CASCADE)
     values = models.FileField(blank=True, null=True)
